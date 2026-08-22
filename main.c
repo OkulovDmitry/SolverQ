53df8cc919c6a4b3a1650d6c457440b83f6ba1ce
OkulovDmitriy <okulovd.mipt.@gmail.com>
2026-08-22 19:44:56 +0300

    Добавил структуры и разбил solve

--- main.c
+++ main.c
@@ -10,12 +10,23 @@ enum boolean {FALSE, TRUE};
 #define EPSILON 1e-10
 #define MAX_ACCURACY 10
 
+struct Quadratic_equation_param
+{
+    double coefficients[3];
+    double roots[2];
+    int number_of_roots;
+};
+
+//void run_tests();
+//int run_one_test();
 int turn_on();
-void greeting(double coefficients_ptr[]);
+void greeting(struct Quadratic_equation_param *parameters_ptr);
 void clear_buffer();
-int solve(double coefficients_ptr[], double roots_ptr[]);
-int solve_linear(double coefficients_ptr[], double roots_ptr[]);
-int output(int number_of_roots, double roots_ptr[]);
+int check_buffer();
+int solve(struct Quadratic_equation_param *parameters_ptr);
+int solve_linear(struct Quadratic_equation_param *parameters_ptr);
+int solve_not_linear(struct Quadratic_equation_param *parameters_ptr);
+int output(struct Quadratic_equation_param *parameters_ptr);
 int compare(double x, double y);
 int equality_check(double x, double y);
 double convert_to_zero(double x);
@@ -23,26 +34,54 @@ double convert_to_zero(double x);
 int main()
 {
     printf("[------------------------------Ultra Solver for quadratic equations by OKD------------------------------]\n");
+
+    //run_tests();
+
     while(turn_on())
     {
-        double coefficients[3];
-        double roots[2];
+        struct Quadratic_equation_param parameters= {0};;
 
-        greeting(coefficients);
+        greeting(&parameters);
 
-        assert(finite(coefficients[0]));
-        assert(finite(coefficients[1]));
-        assert(finite(coefficients[2]));
+        assert(finite((parameters.coefficients)[0]) && "ERROR"); // спросить
+        assert(finite((parameters.coefficients[1])) && "ERROR");
+        assert(finite((parameters.coefficients[2])) && "ERROR");
 
-        int number_of_roots = solve(coefficients, roots);
+        parameters.number_of_roots = solve(&parameters);
 
-        output(number_of_roots, roots);
+        output(&parameters);
     }
 
     printf("Thank you");
 
     return 0;
 }
+
+/*void run_tests()
+{
+    char test_choice = 'n';
+
+    printf("Do you want to run unit-tests?\n(y\\n)? ");
+    while(scanf("%c", &test_choice) != 1 || check_buffer())
+    {
+        printf("ERROR: Incorrect format\n");
+        printf("Enter your choice again: ");
+        clear_buffer();
+    }
+
+    if (test_choice == 'y')
+    {
+        run_one_test({1, 2, 1}, {-1, NAN});
+    }
+}
+
+int run_one_test(double a, double b, double c, int  double x1_test,  double x2_test)
+{
+    double test_coefficients[3] = {a, b, c};
+    double test_roots[2] = {x1_test, x2_test};
+
+
+}*/
 
 int turn_on()
 {
@@ -51,30 +90,35 @@ int turn_on()
     while(scanf("%i", &choice) != 1 || (choice != 0 && choice != 1) || check_buffer())
     {
         printf("ERROR: Incorrect format\n");
-        printf("Enter choice again:");
+        printf("Enter choice again: ");
         clear_buffer();
     }
 
-    assert(finite(choice) != 0);
+    assert(finite(choice) != 0 && "ERROR");
 
     return choice;
 }
 
-void greeting(double coefficients_ptr[])
+void greeting(struct Quadratic_equation_param *parameters_ptr)
 {
-    assert(coefficients_ptr != NULL);
+    assert(&parameters_ptr->coefficients != NULL && "ERROR");
+    assert(&parameters_ptr->roots != NULL && "ERROR");
+    assert(&parameters_ptr->number_of_roots != NULL && "ERROR");
 
     printf("Enter coefficients: ");
-    while (scanf("%lg %lg %lg", &coefficients_ptr[0], &coefficients_ptr[1], &coefficients_ptr[2]) != 3 || check_buffer())
+    // спросить
+    while (scanf("%lg %lg %lg", &(parameters_ptr->coefficients)[0],
+                                &(parameters_ptr->coefficients)[1],
+                                &(parameters_ptr->coefficients)[2]) != 3 || check_buffer())
     {
         printf("ERROR: Incorrect format\n");
         printf("Enter coefficients again: ");
         clear_buffer();
     }
 
-    assert(finite(coefficients_ptr[0]));
-    assert(finite(coefficients_ptr[1]));
-    assert(finite(coefficients_ptr[2]));
+    assert(finite((parameters_ptr->coefficients)[0]));
+    assert(finite((parameters_ptr->coefficients)[1]));
+    assert(finite((parameters_ptr->coefficients)[2]));
 }
 
 void clear_buffer()
@@ -87,33 +131,47 @@ int check_buffer()
     return (buffer != '\n') ? TRUE : FALSE;
 }
 
-int solve(double coefficients_ptr[], double roots_ptr[])
+int solve(struct Quadratic_equation_param *parameters_ptr)
 {
-    assert(coefficients_ptr != NULL);
+    assert(&parameters_ptr->coefficients != NULL);
+    assert(&parameters_ptr->roots != NULL);
+    assert(&parameters_ptr->number_of_roots != NULL);
 
-    if (equality_check(coefficients_ptr[0], 0))
+    if (equality_check((parameters_ptr->coefficients)[0], 0))
     {
-        solve_linear(coefficients_ptr, roots_ptr);
+        return solve_linear(parameters_ptr);
     }
     else
     {
-        double discr = coefficients_ptr[1]*coefficients_ptr[1] - 4*coefficients_ptr[0]*coefficients_ptr[2];
+        return solve_not_linear(parameters_ptr);
+    }
+}
+
+int solve_not_linear(struct Quadratic_equation_param *parameters_ptr)
+{
+    //спросить
+    assert(&parameters_ptr->coefficients != NULL);
+    assert(&parameters_ptr->roots != NULL);
+    assert(&parameters_ptr->number_of_roots != NULL);
+
+    double discr = (parameters_ptr->coefficients)[1]*(parameters_ptr->coefficients)[1] - 4*(parameters_ptr->coefficients)[0]*(parameters_ptr->coefficients)[2];
 
         if (compare(discr, 0))
         {
-            double raw_x1 = (-coefficients_ptr[1] + sqrt(discr))/(2*coefficients_ptr[0]);
-            double raw_x2 = (-coefficients_ptr[1] - sqrt(discr))/(2*coefficients_ptr[0]);
+            //подумать
+            double raw_x1 = (-(parameters_ptr->coefficients)[1] + sqrt(discr))/(2*(parameters_ptr->coefficients)[0]);
+            double raw_x2 = (-(parameters_ptr->coefficients)[1] - sqrt(discr))/(2*(parameters_ptr->coefficients)[0]);
 
-            roots_ptr[0] = convert_to_zero(raw_x1);
-            roots_ptr[1] = convert_to_zero(raw_x2);
+            (parameters_ptr->roots)[0] = convert_to_zero(raw_x1);
+            (parameters_ptr->roots)[1] = convert_to_zero(raw_x2);
 
             return 2;
         }
         else if (equality_check(discr, 0))
         {
-            double raw_x = -coefficients_ptr[1]/(2*coefficients_ptr[0]);
+            double raw_x = -(parameters_ptr->coefficients)[1]/(2*(parameters_ptr->coefficients)[0]);
 
-            roots_ptr[0] = equality_check(raw_x, 0) ? 0 : raw_x;
+            (parameters_ptr->roots)[0] = equality_check(raw_x, 0) ? 0 : raw_x;
 
             return 1;
         }
@@ -121,44 +179,51 @@ int solve(double coefficients_ptr[], double roots_ptr[])
         {
             return NO_ROOTS;
         }
-    }
 }
 
-int solve_linear(double coefficients_ptr[],double roots_ptr[])
+int solve_linear(struct Quadratic_equation_param *parameters_ptr)
 {
-    if (equality_check(coefficients_ptr[1], 0))
+    assert(&parameters_ptr->coefficients != NULL);
+    assert(&parameters_ptr->roots != NULL);
+    assert(&parameters_ptr->number_of_roots != NULL);
+
+    if (equality_check((parameters_ptr->coefficients)[1], 0))
     {
-        return (equality_check(coefficients_ptr[2], 0)) ? INF_ROOTS : NO_ROOTS;
+        return (equality_check((parameters_ptr->coefficients)[2], 0)) ? INF_ROOTS : NO_ROOTS;
     }
     else
     {
-        double raw_x = -coefficients_ptr[2]/coefficients_ptr[1];
-        roots_ptr[0] = convert_to_zero(raw_x);
+        double raw_x = -(parameters_ptr->coefficients)[2]/(parameters_ptr->coefficients)[1];
+        (parameters_ptr->roots)[0] = convert_to_zero(raw_x);
 
         return 1;
     }
 }
 
-int output(int number_of_roots, double roots_ptr[])
+int output(struct Quadratic_equation_param *parameters_ptr)
 {
-    assert(finite(roots_ptr[0]));
-    assert(finite(roots_ptr[1]));
+    assert(&parameters_ptr->coefficients != NULL);
+    assert(&parameters_ptr->roots != NULL);
+    assert(&parameters_ptr->number_of_roots != NULL);
 
-    switch(number_of_roots)
+    assert(finite((parameters_ptr->roots)[0]));
+    assert(finite((parameters_ptr->roots)[1]));
+
+    switch(parameters_ptr->number_of_roots)
     {
         case NO_ROOTS: printf("No roots\n");
                 break;
 
-        case ONE_ROOT: printf("This equation has one root: %.*g\n", MAX_ACCURACY, roots_ptr[0]);
+        case ONE_ROOT: printf("This equation has one root: %.*g\n", MAX_ACCURACY, (parameters_ptr->roots)[0]);
                 break;
 
-        case TWO_ROOTS: printf("This equation has two roots: %.*g and %.*g\n", MAX_ACCURACY, roots_ptr[0], MAX_ACCURACY, roots_ptr[1]);
+        case TWO_ROOTS: printf("This equation has two roots: %.*g and %.*g\n", MAX_ACCURACY, (parameters_ptr->roots)[0], MAX_ACCURACY, (parameters_ptr->roots)[1]);
                 break;
 
         case INF_ROOTS: printf("This equation has infinite number of roots.\n");
                 break;
 
-        default: printf("ERROR: number_of_roots = %i\n", &number_of_roots);
+        default: printf("ERROR: number_of_roots = %i\n", parameters_ptr->number_of_roots);
     }
 }
 
